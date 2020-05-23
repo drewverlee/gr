57518e63fc4a1d3892388add7b6635f9f792cc14
drewverlee <drew.verlee@gmail.com>
2020-05-23 09:54:03 -0500

    use git hub repo name & add option to add remote

--- src/gr.clj
+++ src/gr.clj
@@ -4,12 +4,17 @@
   (:require [clojure.string :as str]
             [babashka.curl :as curl]
             [cheshire.core :as json]
+            [clojure.java.shell :as shell]
             [clojure.java.io :as io]))
 
-(defn cwd []
-  (-> (System/getProperty "user.dir")
-    (str/split #"/")
-    last))
+(def repo (-> "git"
+            (shell/sh "rev-parse" "--show-toplevel")
+            :out
+            (str/split #"/")
+            last
+            (str/trim)))
+
+(def cmd (str "git remote add origin git@github.com:drewverlee/" repo ".git"))
 
 (defn create-github-repo!
   [name]
@@ -17,8 +22,9 @@
     {:headers {"Authorization" (str "token " (System/getenv "GITHUB_TOKEN"))}
      :body    (json/generate-string {"name" name})}))
 
-;; creates a new github repo with the name of the current
-;;directory
-(->>
-  (cwd)
-  create-github-repo!)
+(create-github-repo! repo)
+(println cmd)
+
+(when *command-line-args*
+  (println "adding remote!")
+  (apply shell/sh (str/split cmd #" ")))
